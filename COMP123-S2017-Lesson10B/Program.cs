3a59602f327e4048b515a95ed2192dda6139b7e6
Tom Tsiliopoulos <tsiliopoulos@hotmail.com>
2017-07-18 18:17:26 -0400

    Implemented the SuperVillain > and < overloaded operator methods in the Driver class

--- COMP123-S2017-Lesson10B/Program.cs
+++ COMP123-S2017-Lesson10B/Program.cs
@@ -8,7 +8,7 @@ using System.Threading.Tasks;
  * Name: Tom Tsiliopoulos
  * Date: July 11, 2017
  * Description: Demo for Lesson 10
- * Version: 0.8 - Implemented the SuperVillain class
+ * Version: 0.9 - Implemented the SuperVillain > and < overloaded operator methods
  */
 
 namespace COMP123_S2017_Lesson10B
@@ -33,8 +33,22 @@ namespace COMP123_S2017_Lesson10B
             Console.WriteLine();
             Console.WriteLine("Malice: " + superVillain.Malice);
 
+            if(superVillain.GetPower("Shadow Casting") > 
+                superVillain.GetPower("Combat Senses"))
+            {
+                Console.WriteLine("Shadow Casting is more powerful than Combat Senses");
+            }
 
-           
+            if (superVillain.GetPower("Combat Senses") >
+                superVillain.GetPower("Mind Control"))
+            {
+                Console.WriteLine("Combat Senses is more powerful than Mind Control");
+            }
+            else
+            {
+                Console.WriteLine("Combat Senses is weaker than Mind Control");
+            }
+
         }
     }
 }
